0e00037557bb558e8da6d20c5cb7d3f0a168e174
shuvo-99 <anontoshuvo99@gmail.com>
2023-12-18 02:26:23 +0600

    new problem

--- HackerRank/New Companies.sql
+++ HackerRank/New Companies.sql
@@ -19,3 +19,20 @@ GROUP BY
     c.company_code, c.founder
 ORDER BY
     c.company_code;
+
+
+----- ALternative -----
+
+select 
+        c.company_code, 
+        c.founder, 
+        count(distinct(lead_manager_code)) as lmc, 
+        count(distinct(senior_manager_code)) as smc, 
+        count(distinct(manager_code)) as mc, 
+        count(distinct(employee_code)) as ec
+from Employee as e
+inner join
+Company as c
+on e.company_code=c.company_code
+group by c.company_code, c.founder
+order by c.company_code;
